53f2be4d35e43b9978cca4f617707015a3dd9c51
Karolina606 <nogacka.karolina@gmail.com>
2022-10-22 13:23:37 +0200

    add views for login and registration

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,18 +1,45 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import AuthContext from "../context/AuthContext";
+import { TextField, Button, Box, Grid } from '@mui/material';
+import {makeStyles} from '@material-ui/core';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: theme.spacing(2),
+'& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '300px',
+    },
+'& .MuiButtonBase-root': {
+      margin: theme.spacing(2),
+    },
+  },
+}));
 
 const LoginPage = () => {
   const { loginUser } = useContext(AuthContext);
+
+  const classes = useStyles();
+  // create state variables for each input
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   const handleSubmit = e => {
     e.preventDefault();
-    const username = e.target.username.value;
-    const password = e.target.password.value;
+    // const username = e.target.username.value;
+    // const password = e.target.password.value;
     username.length > 0 && loginUser(username, password);
   };
 
+
   return (
     <section>
-      <form onSubmit={handleSubmit}>
+      {/* <form onSubmit={handleSubmit}>
         <h1>Login </h1>
         <hr />
         <label htmlFor="username">Username</label>
@@ -20,7 +47,60 @@ const LoginPage = () => {
         <label htmlFor="password">Password</label>
         <input type="password" id="password" placeholder="Enter Password" />
         <button type="submit">Login</button>
-      </form>
+      </form> */}
+    <Box sx={{px:"1rem"}}>
+    <h1>Zaloguj się</h1>
+      <form className={classes.root} onSubmit={handleSubmit}>
+      <TextField
+        label="Nazwa użytkownika"
+        variant="outlined"
+        required
+        value={username}
+        onChange={e => setUsername(e.target.value)}
+      />
+      <TextField
+        label="Hasło"
+        variant="outlined"
+        type="password"
+        required
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <div>
+        <Button type="submit" variant="contained">
+          Logowanie
+        </Button>
+      </div>
+
+      {/* <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+      <Grid item xs={12}>
+        <TextField
+          label="Nazwa użytkownika"
+          variant="outlined"
+          required
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          label="Hasło"
+          variant="outlined"
+          type="password"
+          required
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Button type="submit" variant="contained">
+            Logowanie
+        </Button>
+      </Grid>
+    </Grid> */}
+
+    </form>
+    </Box>
     </section>
   );
 };
